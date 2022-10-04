e1e2407ea7593b5d03d9d5ae3405449d7ed28a36
ilya <ilya_1_3_5@mail.ru>
2022-10-04 23:51:44 +0500

    5 task added

---
+++ seninar2/task5/Program.cs
@@ -0,0 +1,21 @@
+﻿// int Prompt(string msg)
+// {
+//     Console.WriteLine(msg);                               //ввод
+//     int Number = Convert.ToInt32(ConsoleReadLine());      //присвоение
+//     return number;                                        //возврат к целому
+// }
+
+
+int a = new Random().Next(10, 10000);
+Console.WriteLine(a);
+int max = a % 10;
+while (a > 0)
+{
+    int temp = a% 10;
+    if(temp > max)
+    {
+        max = temp;
+    }
+    a = a / 10;
+}
+Console.WriteLine(max);
