d81842509d3d59675bfa2f2395f69148ab242533
ilya <ilya_1_3_5@mail.ru>
2022-10-04 16:22:10 +0500

    3task

---
+++ lec1/3task/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+int n = array.Length; // длинна массива
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать цикл
+    }
+    index++;
+}
