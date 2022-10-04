96c7b082ea890c2df34206796d393a819b123361
ilya <ilya_1_3_5@mail.ru>
2022-10-04 22:56:02 +0500

    1 2 tasks added

---
+++ seninar2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВедите трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 99 && n < 1000) 
+{
+    Console.WriteLine("Число не трехзначное");
+}
+else
+{
+    int ost = n % 10;
+    Console.WriteLine(ost);
+}
+
---
+++ seninar2/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+if (a%10 > a / 10)
+{
+    Console.WriteLine($"{a%10} нибольшее число");
+}
+else
+{
+    Console.WriteLine($"{a/10} нибольшее число");
+}
