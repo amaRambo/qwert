c8d053c7ee5004ad9e4cdfdd4686e2f41053cc8a
ilya <ilya_1_3_5@mail.ru>
2022-10-04 16:44:11 +0500

    4task

---
+++ lec1/4task/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[] collection) // метод void(этот метод ничего не возвращает) наименование_метода (тип[] аргумент)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10]; // создай 10 элементов, и по умолчанию он заполнен нулями
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
